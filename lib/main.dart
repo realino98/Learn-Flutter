e603acbd671c6b5fc38a254b34a6b4752aa5443c
Bernard <bernard.realino98@gmail.com>
2023-06-08 14:04:59 +0700

    Paging

--- lib/main.dart
+++ lib/main.dart
@@ -12,71 +12,96 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyAppExt());
+  }
+}
+
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
   String buttonName = "CLICK";
   int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World'),
+      ),
+      body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: Colors.red,
+                            primary: Colors.amber,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              buttonName = "Clicked";
+                            });
+                          },
+                          child: Text(buttonName)),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const NextPage(),
+                              ),
+                            );
+                          },
+                          child: Text("Move to Next Page")),
+                    ],
+                  ),
+                )
+              : Image.asset('images/Background.jpeg')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Setting",
+            icon: Icon(
+              Icons.settings,
+            ),
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World'),
-        ),
-        body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.red,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              onPrimary: Colors.red,
-                              primary: Colors.amber,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                buttonName = "Clicked";
-                              });
-                            },
-                            child: Text(buttonName)),
-                        ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                buttonName = "Clicked";
-                              });
-                            },
-                            child: Text(buttonName)),
-                      ],
-                    ),
-                  )
-                : Image.asset('images/Background.jpeg')),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: "Home",
-              icon: Icon(
-                Icons.home,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "Setting",
-              icon: Icon(
-                Icons.settings,
-              ),
-            ),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
